fa597600deaf73757e9f88f5775695fc18e9f412
Milenna Alves <milennafeijocanal@gmail.com>
2026-08-24 21:16:43 -0300

    feat: inicialização do projeto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-mint-500 w-[500px] flex justify-center">
+      <div className="bg-blue-500 w-40 h-40 m-4"></div>
+      <div className="bg-red-500 w-40 h-40 m-4"></div>
+    </div>
+  )
+}
+
+export default App;
