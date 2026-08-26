e296e59090fc298a4684113810887563555ba2c8
Milenna Alves <milennafeijocanal@gmail.com>
2026-08-26 20:45:31 -0300

    fix: arruma padding horizontal para responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
   <main className="bg-navy-950 h-screen">
     <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
-      <div className="max-w-175 m-auto mb-6">
+      <div className="max-w-175 m-auto mb-6 px-6">
         <TodoHeader />
         <TodoForm />
       </div>
