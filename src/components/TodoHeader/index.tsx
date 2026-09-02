be3cfc83ec8fd929d852afd6f0ee362c8ec9aef6
Milenna Alves <milennafeijocanal@gmail.com>
2026-09-02 15:04:37 -0300

    fix: arruma responsividade com mudanças nos botões inferiores

--- src/components/TodoHeader/index.tsx
+++ src/components/TodoHeader/index.tsx
@@ -8,7 +8,7 @@ const TodoHeader = () => {
     const { theme, toggleTheme } = useContext(ThemeContext);
   
     return (<header className="flex justify-between pt-20 mb-14" >
-        <h1 className="text-white text-[2.5rem] font-bold tracking-[1rem]">TODO</h1>
+        <h1 className="text-white text-3xl sm:text-[2.5rem] font-bold tracking-[1rem]">TODO</h1>
 
         <button className="cursor-pointer"onClick={toggleTheme}>
             <img className="w-8 h-8" src={themeConfig[theme].icon} alt="Toggle Theme" />
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,45 +1,56 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { themeConfig } from "../../contexts/theme";
- 
- const todos = [
-        { id: 1, text: "Todo 1" },
-        { id: 2, text: "Todo 2" },
-        { id: 3, text: "Todo 3" },
-    ]
 
-const TodoList = () => { 
-    
+const todos = [
+    { id: 1, text: "Todo 1" },
+    { id: 2, text: "Todo 2" },
+    { id: 3, text: "Todo 3" },
+]
+
+const TodoList = () => {
+
     const { theme } = useContext(ThemeContext);
 
     return (
 
+        <>
+            <div className={themeConfig[theme].todo.backgroundColor + " rounded-md"}>
 
-        <div className={themeConfig[theme].todo.backgroundColor + " rounded-md"}>
-            <ul>
-                {todos.map((todo) => (
-                    <li className={`border-b ${themeConfig[theme].todo.borderColorList} p-6`} key={todo.id}>
-                        <div className="flex items-center gap-5">
-                            <button className="w-6 h-6 border border-navy-850 rounded-full cursor-pointer"></button>
-                            <p className={themeConfig[theme].todo.textColor}>{todo.text}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+                <ul>
+                    {todos.map((todo) => (
+                        <li className={`border-b ${themeConfig[theme].todo.borderColorList} p-6`} key={todo.id}>
+                            <div className="flex items-center gap-5">
+                                <button className="w-6 h-6 border border-navy-850 rounded-full cursor-pointer"></button>
+                                <p className={themeConfig[theme].todo.textColor}>{todo.text}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
 
-            <div className="text-sm flex justify-between items-center p-6">
-                <p className={themeConfig[theme].todo.textColor + " cursor-pointer"}>{todos.length} items total</p>
+                <div className="text-sm flex justify-between items-center p-6">
+                    <p className={themeConfig[theme].todo.textColor + " cursor-pointer"}>{todos.length} items total</p>
 
-                <div className="flex gap-4">
-                    <button className="text-bright-blue cursor-pointer">All</button>
-                    <button className={themeConfig[theme].todo.textColor + " cursor-pointer"}>Active</button>
-                    <button className={themeConfig[theme].todo.textColor + " cursor-pointer"}>Completed</button>
+                    <div className="hidden sm:flex gap-4">
+                        <button className="text-bright-blue cursor-pointer">All</button>
+                        <button className={themeConfig[theme].todo.textColor + " cursor-pointer"}>Active</button>
+                        <button className={themeConfig[theme].todo.textColor + " cursor-pointer"}>Completed</button>
+                    </div>
+
+                    <button className={themeConfig[theme].todo.textColor + " cursor-pointer"}>Clear Completed</button>
                 </div>
 
-                <button className={themeConfig[theme].todo.textColor + " cursor-pointer"}>Clear Completed</button>
+
+
             </div>
 
-        </div>
+            <div className={themeConfig[theme].todo.backgroundColor + " rounded-md flex justify-center gap-4 p-4 mt-6 sm:hidden"}>
+                <button className="text-bright-blue cursor-pointer">All</button>
+                <button className={themeConfig[theme].todo.textColor + " cursor-pointer"}>Active</button>
+                <button className={themeConfig[theme].todo.textColor + " cursor-pointer"}>Completed</button>
+            </div>
+
+        </>
 
     )
 }
